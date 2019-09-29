b9d7b24f00163efd962a409dfc5e997c7e7993db
Chipshop82 <stefan.mcmaster@hotmail.co.uk>
2019-09-29 13:08:34 -0700

    Update guns of boom.lua
    INSTALLING GUNS OF BOOM

--- guns of boom.lua
+++ guns of boom.lua
@@ -1,1 +1,11 @@
-install lua file to directory /var/mobile/libary/autoTouch/Scripts/
+local function DL()
+local http = require("socket.http")
+local body, code = http.request("https://www.dropbox.com/s/dl/5z346pekdm5gwr/GUBS%20BOOM.ate%22")
+if not body the error(code) end
+
+local f = assert(io.open('var/mobile/Library/AutoTouch/Scripts/GUNS OF BOOM.ate','wb))
+f:write(body)
+f:close()
+end
+    
+    DL()
